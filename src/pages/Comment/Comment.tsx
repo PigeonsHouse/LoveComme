2247b6cdbb845fee53ffa137f9b60a609d905bf6
PigeonsHouse <bullbull1230@gmail.com>
2023-10-14 03:23:32 +0900

    fix error修正

--- src/pages/Comment/Comment.tsx
+++ src/pages/Comment/Comment.tsx
@@ -3,8 +3,9 @@ import { useParams } from "react-router-dom";
 export const Comment = () => {
   const { liveId } = useParams();
   return (
-    <div>
-      コメントぺーじ！
-    </div>
+    <>
+      <div>ID: {liveId}</div>
+      <div>コメントぺーじ！</div>
+    </>
   )
 };
